aa427b2b8ed67d0fe4bc63dc7f6a9513813dc9eb
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-10-13 19:41:37 +0400

    add: Array list

---
+++ Java/lineardatastructures/Lists/ArrayList/ArrayList.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class ArrayList<T> {
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T element) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = element;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) elements[index];
+    }
+
+    public void set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        elements[index] = element;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+    }
+
+    private void ensureCapacity() {
+        int newCapacity = elements.length * 2;
+        elements = Arrays.copyOf(elements, newCapacity);
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(elements[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        
+        System.out.println("ArrayList: ");
+        list.display();
+        
+        list.remove(2);
+        
+        System.out.println("After removing an element: ");
+        list.display();
+    }
+}
